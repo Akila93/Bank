61a8b0ba78845fb3b214e69da954986af070ab2d
Akila <akilachathuranga999@gmail.com>
2023-03-10 04:48:28 +0800

    test cases files added

---
+++ src/test/java/com/app/bank/TransactionServiceTests.java
@@ -0,0 +1,10 @@
+package com.app.bank;
+
+import org.aspectj.lang.annotation.Before;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TransactionServiceTests {
+
+
+}
